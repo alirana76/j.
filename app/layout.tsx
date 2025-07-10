b6a3e4e9338245330d4d7963ee34857ec8c9a847
alirana76 <alihaiderrana846@gmail.com>
2025-07-10 16:32:22 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Junaid Jamshed - Clothing Store',
+  description: 'Created with Ali Rana',
+  generator: 'next.js',
 }
 
 export default function RootLayout({
